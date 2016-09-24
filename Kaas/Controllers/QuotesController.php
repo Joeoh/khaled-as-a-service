636082f9f709abd98954511121f1957473fd3707
Joe O'Hara <joe.is.ohara@gmail.com>
2016-09-24 16:44:06 +0100

    Adding Quotes model/controller and enable eloquent

---
+++ Kaas/Controllers/QuotesController.php
@@ -0,0 +1,15 @@
+<?php
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+/**
+ * Created by PhpStorm.
+ * User: joe
+ * Date: 24/09/2016
+ * Time: 16:40
+ */
+class QuotesController extends BaseController
+{
+    public function getRandomQuote() : Quote {
+
+    }
+}
---
+++ Kaas/Models/Quotes.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: joe
+ * Date: 24/09/2016
+ * Time: 16:42
+ */
+class Quotes extends \Illuminate\Database\Eloquent\Model
+{
+
+}
